344195e787561154de2e9149edb89d9221b6977b
Grygoriy Bezshaposhnikov <gbezsh@gmail.com>
2022-07-13 16:19:53 -0500

    fix layers/create welcome div/set hover styles

--- src/main.js
+++ src/main.js
@@ -8,12 +8,21 @@ const {GenerateInfo, loadRegions, loadProduction} = loaders;
 /**
  * jquery selectors
  */
-let table = $('#flex-table');
+let $table = $('#flex-table');
+let $welcome = $('.welcome');
 
 //array to filter the highlighted regions
 const producers = ["Kyiv", "Odesa", "Lviv", "Mykolaiv", "Dnipro", "Zaporizhia", "Kherson", "Vinnytsia", "Donets'k", "Luhans'k", "Kharkiv"];
 // cuurent consumers highlighted
-let consumersLayer = L.featureGroup();
+let currentLayer = undefined;
+let defaultStyle = {
+  color: '#87ceeb',
+  fillOpacity: 0.1
+};
+let hoverStyle ={
+  color: '#ff7d00',
+  fillOpacity: 0.5
+};
 
 var map = L.map('map', {
   zoomControl: false
@@ -24,41 +33,45 @@ var tiles = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{
   attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+/**
+ * returns region info string
+ * @param feature 
+ * @returns string with region info
+ */
+const setRegionInfo = (feature) => {
+  if(feature.properties.republic === "Ukraine") {
+    return feature.properties.name + ' RES';
+  } else {
+    return feature.properties.name;
+  }
+}
+
 let regionsJSON = await loadRegions().then(data => regionsJSON = data);
 let regionsLayer = L.geoJSON(regionsJSON, {
-  style: function(geoJsonFeature){ 
-    return {
-      color: '#87ceeb',
-      fillOpacity: 0.1
-    };
-  },
+  style: defaultStyle,
   onEachFeature: function(feature, layer) {
-    let html = undefined;
-    if(feature.properties.republic === "Ukraine") {
-      html = '<h1>' + feature.properties.name + ' Regional Economic Soviet' + '</h1>';
-    } else {
-      html = '<h1>' + feature.properties.name + '</h1>';
-    }
-    layer.bindPopup(html);
     layer.on('click', function(){
       let currentRegion = feature.properties.name;
       if(producers.includes(currentRegion)) {
+        $table.toggle();
+        $welcome.toggle();
         highlightConsumers(currentRegion);
         GenerateInfo(currentRegion);
       }
     });
     layer.on('mouseover', () => {
-      $('#current-region').val(feature.properties.name);
+      $('#current-region').val(setRegionInfo(feature));
+      layer.setStyle(hoverStyle);
     });
     layer.on('mouseout', () => {
       $('#current-region').val('');
+      layer.setStyle(defaultStyle);
     });
   }
 }).addTo(map);
 
 const highlightConsumers = async (region) => {
   let consumers = new Set();
-  let features = [];
   let data = await loadProduction(region);
   try{
     data.forEach(entry => {
@@ -66,25 +79,31 @@ const highlightConsumers = async (region) => {
         consumers.add(region);
       } else consumers.add(entry.consumer);
     });
-    
-    //!!!
-    //populate new layer with L.geoJSON and FILTER option
-    //!!!
+    //create new layer
+    currentLayer = L.geoJSON(regionsJSON, {
+      style: defaultStyle,
+      filter: function(geoJsonFeature){
+        if(consumers.has(geoJsonFeature.properties.name)) return true;
+      },
+      onEachFeature: function(feature, layer){
+        layer.on('mouseover', () => {
+          $('#current-region').val(setRegionInfo(feature));
+          layer.setStyle(hoverStyle);
+        });
+        layer.on('mouseout', () => {
+          $('#current-region').val('');
+          layer.setStyle(defaultStyle);
+        });
+      }
+    });
     //set map view
-    
     map.flyTo(map.getCenter(), 4);
     //replace the layers
     regionsLayer.remove();
-    consumersLayer.addTo(map);
-    console.log('check end');
-
+    currentLayer.addTo(map);
   }catch(error) {
     console.log(error);
   }
-};
-
-const consumersPopup = () => {
-  return L.popup().setContent('test');
 };
 
 /**
@@ -93,10 +112,17 @@ const consumersPopup = () => {
  */
 const resetView = () => {
   //remove consumers layer
-  consumersLayer.remove();
+  console.log(currentLayer);
+  if(currentLayer !== undefined)
+    currentLayer.remove();
+  //toggle welcome div
+  $welcome.toggle();
+  //toggle info table
+  $table.toggle();
   //delete the generated rows
   let $header = $('.header');
-  $header.siblings().remove();
+  if($header !== undefined)
+    $header.siblings().remove();
   //replace with the base layer
   regionsLayer.addTo(map);
 }
